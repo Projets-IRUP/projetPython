a16ac354353f43556dfa05ccc357f47f1cecba21
Mathis Vercelloni <mvercelloni.mpi23@eleve-irup.com>
2024-04-17 17:21:50 +0200

    Tests et modifs

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 
 ## Site à Scrap : 
 - https://marine.meteoconsult.fr/meteo-marine/horaires-des-marees/biarritz-1085/mars-2024
+
+
+# Ce qu'il faut récupérer : 
+- un port
+- Jour de la semaine
+- Heure Marée basse et haute
+- Coefficient de marée 
+
+partir de grid col-16 tide
+ligne 1827
+
+Convertir les textes en jours de la semaine.
+Convertir les textes en heures de marées
+Convertir les textes en coefficient de marées
+
+boucle sur les ports et une fois sur les mois en cours
--- main.py
+++ main.py
@@ -2,29 +2,62 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-# url ="https://marine.meteoconsult.fr/meteo-marine/horaires-des-marees/port-maria-999/mars-24"
-# try:
+'''
+url ="https://marine.meteoconsult.fr/meteo-marine/horaires-des-marees/port-maria-999/avril-24"
+try:
     
-#     reponse = requests.get(url)
-#     reponse.encoding ="utf-8"
-# except:
-#     requests.post("https://smsapi.free-mobile.fr/sendmsg?user=49185763&pass=3y7dWE7iULpE0Z&msg=Erreur%20API")
+    reponse = requests.get(url)
+    reponse.encoding ="utf-8"
+except:
+    requests.post("https://smsapi.free-mobile.fr/sendmsg?user=49185763&pass=3y7dWE7iULpE0Z&msg=Erreur%20API")
 
-# if reponse.status_code ==200:
-#     html=reponse.text
-#     print(html)
-    # f=open("copy.html","w",encoding='utf-8')
-#     f.write(html)
-#     f.close()
+if reponse.status_code ==200:
+    html=reponse.text
+    print(html)
+    f=open("copy.html","w",encoding='utf-8')
+    f.write(html)
+    f.close()
 html = ""
 # Ouvrir un fichier en mode lecture
 with open("copy.html", "r") as fichier:
     # Lire le contenu du fichier
     html = fichier.read()
-    
+'''
 
-soup = BeautifulSoup(html,'html.parser')
+# Créer un objet BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
 
+# Trouver toutes les balises <div> avec la classe "grid col-16 tide"
+ListeDivGlobal = soup.find_all('div', class_='grid col-16 tide')
+
+
+# Afficher le contenu de chaque balise trouvée
+for div in ListeDivGlobal:
+    print(div)
+    print('************************************')
+
+    # Trouver la balise <div> avec la classe "tide-date week"
+    date_div = soup.find('div', class_='tide-date week')
+
+    # Récupérer le contenu du span
+    date_str = date_div.span.text.strip()
+
+    # Convertir la date en une date valide avec le mois en cours
+    # Obtenez le mois actuel
+    current_month = datetime.now().strftime("%B").lower()
+
+    # Remplacer le nom du mois dans la chaîne de date par le mois actuel
+    date_str = date_str.replace("vendredi", "Friday").replace("samedi", "Saturday").replace("dimanche", "Sunday").replace("lundi", "Monday").replace("mardi", "Tuesday").replace("mercredi", "Wednesday").replace("jeudi", "Thursday").replace("janvier", current_month).replace("février", current_month).replace("mars", current_month)
+
+    # Convertir la chaîne de date en objet datetime
+    date = datetime.strptime(date_str, "%A %d %B")
+
+    # Afficher la date
+    print(date)
+
+
+
+'''
 # maree = soup.find_all("vendredi 22")
 # for child in maree[0].children:
 #     print(child)
@@ -47,4 +80,5 @@ for row in premier_jour_rows:
     print("Hauteurs:", hauteurs)
 
 # else :
-    requests.post("https://smsapi.free-mobile.fr/sendmsg?user=49185763&pass=3y7dWE7iULpE0Z&msg=Erreur%20API")
+requests.post("https://smsapi.free-mobile.fr/sendmsg?user=49185763&pass=3y7dWE7iULpE0Z&msg=Erreur%20API")
+'''
